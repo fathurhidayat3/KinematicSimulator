bbf4483db18d2445fc091e463135e5b8f258ff04
Fathur Hidayat <fathur.hidayat@gmail.com>
2018-10-15 15:19:36 +0700

    finish forward 2Dof

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var line = document.getElementById("line");
 var inputSection = document.querySelectorAll("[data-for]");
 
 var btnRun = document.getElementById('btn-run');
+var btnFwdRun = document.getElementById('btn-fwd-run');
 var btnReset = document.getElementById('btn-reset');
 
 var simX = document.getElementById("input-deg-x");
@@ -68,6 +69,133 @@ btnRun.onclick = function () {
       lineChecker(dragDiv[elCount]);
     }
   }
+}
+
+var count = 0;
+var count1 = 0; var count2 = 0; var teta = 0;
+
+btnFwdRun.onclick = function () {
+  switch (elCount) {
+    case 1: fwd1DoF(); break;
+    case 2: fwd2DoF(); break;
+    default: break;
+  }
+
+  function fwd1DoF() {
+    let teta = parseInt(document.getElementById(`teta${elCount}`).value);
+    let length = parseInt(document.querySelector(`[data-line='line${elCount}']`).value);
+
+    let mov = setInterval(simMovement, 5);
+
+    function simMovement() {
+      if (count == teta) {
+        clearInterval(mov);
+        count = teta;
+      }
+      else {
+        if (count < teta) count++;
+        else if (count > teta) count--;
+
+        let data1DoF = calc1DoF(count, teta, length);
+
+        simX.value = data1DoF.xValue;
+        simY.value = data1DoF.yValue;
+      }
+    }
+  }
+
+  function fwd2DoF() {
+    let teta2 = parseInt(document.getElementById(`teta${elCount}`).value);
+    let teta1 = parseInt(document.getElementById(`teta${elCount - 1}`).value);
+    let length2 = parseInt(document.querySelector(`[data-line='line${elCount}']`).value);
+    let length1 = parseInt(document.querySelector(`[data-line='line${elCount - 1}']`).value);
+
+    // if (teta2 >= teta1) teta = teta2; 
+    // else teta = teta1;
+
+    if (teta2 == 0 || teta1 == 0) {
+      if (count2 > count1) teta = count2;
+      else teta = count1;
+    }
+    else {
+      if (teta2 >= teta1) teta = teta2;
+      else teta = teta1;
+    }
+
+    var mov = setInterval(simMovement, 5);
+
+    function simMovement() {
+      if (count == teta) {
+        clearInterval(mov);
+        teta = 0; count = 0; count2 = teta2; count1 = teta1;
+      }
+      else {
+        count++;
+
+        if (count2 < teta2) count2++;
+        else if (count2 > teta2) count2--;
+
+        if (count1 < teta1) count1++;
+        else if (count1 > teta1) count1--;
+
+        calc1DoF(count1, length1, 1);
+        let data2DoF = calc2Dof(count2, count1, length2, length1);
+
+        simX.value = data2DoF.xValue;
+        simY.value = data2DoF.yValue;
+      }
+    }
+  }
+
+  function calc1DoF(teta, length, ordo = 0) {
+    let eoeX = length * Math.cos(toRadians(teta));
+    let eoeY = length * Math.sin(toRadians(teta));
+
+    let divPrevLeft = parseInt(dragDiv[0].style.left.split('px')[0]);
+    let divPrevTop = parseInt(dragDiv[0].style.top.split('px')[0]);
+
+    dragDiv[elCount - ordo].style.left = eoeX + divPrevLeft + "px";
+    dragDiv[elCount - ordo].style.top = eoeY + divPrevTop + "px";
+
+    lineChecker(dragDiv[elCount - 1]);
+
+    return {
+      xValue: Math.round(eoeX) + divPrevLeft,
+      yValue: Math.round(eoeY) + divPrevTop
+    }
+  }
+
+  function calc2Dof(teta2, teta1, length2, length1, ordo = 0) {
+    let eoeX = length1 * Math.cos(toRadians(teta1)) + length2 * Math.cos(toRadians(teta1 + teta2));
+    let eoeY = length1 * Math.sin(toRadians(teta1)) + length2 * Math.sin(toRadians(teta1 + teta2));
+
+    let divPrevLeft = parseInt(dragDiv[0].style.left.split('px')[0]);
+    let divPrevTop = parseInt(dragDiv[0].style.top.split('px')[0]);
+
+    dragDiv[elCount].style.left = eoeX + divPrevLeft + "px";
+    dragDiv[elCount].style.top = eoeY + divPrevTop + "px";
+
+    simX.value = Math.round(eoeX) + divPrevLeft;
+    simY.value = Math.round(eoeY) + divPrevTop;
+
+    lineChecker(dragDiv[elCount]);
+
+    return {
+      xValue: Math.round(eoeX) + divPrevLeft,
+      yValue: Math.round(eoeY) + divPrevTop
+    }
+  }
+
+}
+
+
+
+function toDegrees(angle) {
+  return angle * (180 / Math.PI);
+}
+
+function toRadians(angle) {
+  return angle * (Math.PI / 180);
 }
 
 btnReset.onclick = function () {
@@ -198,7 +326,7 @@ function lineChecker(elmnt) {
     //   if(dataLineA && dataLineB) {
     //     dataLineA.value = 11;
     //     dataLineB.value = 10;
-  
+
     //     console.log('dari a ' + dataLineA.value + dataLineB.value);
     //   }
     // }
@@ -207,7 +335,7 @@ function lineChecker(elmnt) {
     //   if(dataLineA && dataLineB) {
     //     dataLineA.value = dataLine.distance;
     //     dataLineB.value = 10;
-  
+
     //     console.log('dari b ' + dataLineA.value + dataLineB.value);
     //   }
     // }
@@ -217,11 +345,12 @@ function lineChecker(elmnt) {
 function distanceCalc(moveObj) {
   let xDelta = moveObj.coordinates.x2 - moveObj.coordinates.x1;
   let yDelta = moveObj.coordinates.y2 - moveObj.coordinates.y1;
-  distance = Math.round(Math.sqrt(Math.pow(xDelta, 2) + Math.pow(yDelta, 2))) / 100;
+  distance = Math.round(Math.sqrt(Math.pow(xDelta, 2) + Math.pow(yDelta, 2)));
 
   return { lineId: moveObj.selectLine.id, distance: distance };
 }
 
-function tetaCalc(dataLine) {
-  console.log(dataLine);
+function tetaCalc() {
+  // console.log(dataLine);
+
 }
